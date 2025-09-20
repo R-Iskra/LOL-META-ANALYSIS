5049f0d37ceab9f8db99f22e475b011564c03907
Ryan Iskra <riskra@gmail.com>
2025-09-20 14:27:43 -0600

    Cleaned main to be ready to use

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main():
 
     top = 500
     matches_per_player = 10
-    min_patch = "15.15"
+    collect_min_patch = "15.15"
 
-    #collect_matches(client=client, db_path=raw_db_path, top=top, matches_per_player=matches_per_player, min_patch=min_patch)
+    collect_matches(client=client, db_path=raw_db_path, top=top, matches_per_player=matches_per_player, min_patch=collect_min_patch)
 
     clean_db_path = "cleaned_match_data.db"
     min_duration = None
-    min_path = None
+    clean_min_patch = None
 
-    clean_matches_from_db(raw_db_path=raw_db_path, clean_db_path=clean_db_path, min_duration=min_duration, min_patch=min_patch)
+    clean_matches_from_db(raw_db_path=raw_db_path, clean_db_path=clean_db_path, min_duration=min_duration, min_patch=clean_min_patch)
 
 main()
